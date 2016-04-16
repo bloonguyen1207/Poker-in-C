c058347c89b0505d89b5a348b8fb3ad36280740a
Mai <s3558475@rmit.edu.vn>
2016-04-16 13:23:01 +0700

    player & deal

--- src/demo.c
+++ src/demo.c
@@ -3,7 +3,7 @@
 //
 
 #include <stdio.h>
-#include <locale.h>
+#include <stdlib.h>
 
 enum suit {HEARTS, DIAMONDS, CLUBS, SPADES};
 typedef enum suit Suit;
@@ -24,7 +24,6 @@ char* getSuit(Suit s) {
 };
 
 int main() {
-    setlocale(LC_ALL, "");
     int size = 52;
     int k = 0;
     Card c;
@@ -39,6 +38,27 @@ int main() {
     }
     for (int m = 0; m < size; m++) {
         printf("%s %i \n", getSuit(deck[m].suit), deck[m].rank);
+    }
+
+    int no_player = 2; //number of players
+    int no_card = 2; //number of cards
+
+    Card *player[no_player]; //an array of player (each player is a pointer)
+    for (int i = 0; i < no_player; i++) { //set place for cards that each player holds (refers to)
+        player[i] = malloc(no_card * sizeof(Card));
+    }
+
+    k = 0;
+    for (int i = 0; i < no_card; i++) {
+        for (int j = 0; j < no_player; j++) {
+            player[i][j] = deck[k];
+            k++;
+        }
+    }
+    for (int i = 0; i < no_player; i++) {
+        for (int j = 0; j < no_card; j++) {
+            printf("Player %d: %s %i \n", i + 1, getSuit(player[i][j].suit), player[i][j].rank);
+        }
     }
 }
 
